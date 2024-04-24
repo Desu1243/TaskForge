6f5e441c9695ff8ddea018ed4b0d81daf7952d14
Desu1243 <lubonskimateusz@gmail.com>
2024-04-24 17:21:06 +0200

    project init

---
+++ android/app/src/main/kotlin/com/desu1243/taskforge/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.desu1243.taskforge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "TaskForge",
+    home: Scaffold(),
+  ));
+}
